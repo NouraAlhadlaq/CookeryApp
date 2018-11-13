6242642054cab53ea777d14cc008fc81228574b0
NouraAlhadlaq <noura6.1994@gmail.com>
2018-11-13 11:48:28 +0300

    home page

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
   get "ingredients/new"
   devise_for :users
-  # root "recipes#index"
-  root "pages#home"
+  root "recipes#index"
+  # root "pages#home"
   resources :users, only: [:edit, :show]
   resources :recipes, :comments, :ingredients
 end
